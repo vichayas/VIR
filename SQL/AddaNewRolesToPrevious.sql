3dbbcd63156d303bc7a9f18b61044f9a5445b45f
Unknown <vichayas@viriyah.co.th>
2018-01-10 16:51:52 +0700

    Update to store procedure

--- SQL/AddaNewRolesToPrevious.sql
+++ SQL/AddaNewRolesToPrevious.sql
@@ -1,251 +1,207 @@
-select * from Agreement where ReferenceNumber = '18181/END/000008-520'
-select * 
+
+DECLARE @Agreement_Id uniqueidentifier
+DECLARE @InAppSource_Id uniqueidentifier
+DECLARE @InAppTarget_Id uniqueidentifier
+DECLARE @Policy_Ref nvarchar(30)
+DECLARE @End_sequence int
+DECLARE @ReferenceNumber nvarchar(30) = '18181/END/000001-563'
+
+DECLARE @numCur int
+DECLARE @numParent int
+
+SELECT @InAppSource_Id = b.Id,@Agreement_Id = a.Id,  @InAppTarget_Id = b.ParentId, @End_sequence = b.END_Sequence, @Policy_Ref = PolicyNumber
+FROM Agreement a
+INNER JOIN InsuranceApplication b on (a.InsuranceApplication_Id = b.Id)
+WHERE a.ReferenceNumber = @ReferenceNumber
+
+select a.ReferenceNumber,a2.ReferenceNumber, iai.PolicyNumber, c1.DescriptionTH, c2.DescriptionTH, iai.END_Sequence
+ from Agreement a
+ inner join  Agreement a2 on (a.InsuranceApplication_Id = a2.InsuranceApplication_Id and a2.Discriminator = 'InsuranceEndorsement')
+ inner join ApplicantEndorsementItem b on (a.Id = b.ApplicantEndorsement_Id)
+ inner join Endorsement c1 on (a.ApplicantEndorsementCategoryId = c1.Id)
+ inner join Endorsement c2 on (b.EndorsementType_Id = c2.Id)
+ inner join InsuranceApplication iai on (iai.Id = a.InsuranceApplication_Id)
+  where a2.ReferenceNumber = @ReferenceNumber
+
+--=== Parent RefernecNumber
+SELECT top 1 * FROM Agreement WHERE InsuranceApplication_Id = @InAppTarget_Id order by ModifiedDate DESC
+
+--== PartyRoles of Current
+select @numCur = count(1) 
 from PartyRole 
-where InsuranceApplication_Id = 'D5DED73D-E058-464D-AF6C-A8BC360DAC11' and -- Endorse
+where InsuranceApplication_Id = @InAppSource_Id and -- Current Policy/Endorse
 Party_Id is not null and
 [Type_Id] is not null 
 
----================== Insurpolicy
+--== PartyRoles of Insurance
+IF @End_sequence = 1
+	BEGIN
+		select @numParent = count(a.Id) 
+		from PartyRole a
+		inner join Agreement b on (b.InsuranceApplication_Id = a.InsuranceApplication_Id)
+		where ReferenceNumber = @Policy_Ref and b.Discriminator = 'BaseInsurancePolicy' and -- Parent of Current Policy/Endorse
+		Party_Id is not null and
+		a.[Type_Id] is not null 
+	END
+ELSE
+	BEGIN
+		select @numParent = count(a.Id) 
+		from PartyRole a
+		inner join Agreement b on (b.InsuranceApplication_Id = a.InsuranceApplication_Id)
+		where ReferenceNumber = @Policy_Ref and b.Discriminator = 'InsurancePolicy' and -- Parent of Current Policy/Endorse
+		Party_Id is not null and
+		a.[Type_Id] is not null 
+	END
 
-select * from Agreement where ReferenceNumber = '18181/POL/000005-520'
-select * 
-from PartyRole 
-where InsuranceApplication_Id = 'DCAB27EB-C824-456E-AA44-83AB17DA402C' and --Insurpolicy
-Party_Id is not null and
-[Type_Id] is not null 
-and Party_Id = 'cae7a555-38f8-4038-94b0-c2f450011d7f'
+print @Agreement_Id
+print @InAppSource_Id
+print @InAppTarget_Id
+print @numCur
+print @numParent
 
----================== BaseInsurancePolicy
-select * from Agreement where InsuranceApplication_Id = '64e2844f-62fc-4785-aa9c-6f7726975d39' 
-select * 
-from PartyRole 
-where InsuranceApplication_Id = '64e2844f-62fc-4785-aa9c-6f7726975d39' and --BaseInsurancePolicy
-Party_Id is not null and
-[Type_Id] is not null and
-Party_Id = 'cae7a555-38f8-4038-94b0-c2f450011d7f'
+IF @numCur > @numParent
+	BEGIN
+			--============ Find the people not in Parent
+			select * 
+			INTO #PartyRole
+			from PartyRole 
+			where InsuranceApplication_Id = @InAppSource_Id and --Current policy/endorse
+			Party_Id is not null and
+			[Type_Id] is not null
+			and Party_Id not in 
+			(
+			select Party_Id 
+			from PartyRole 
+			where InsuranceApplication_Id = @InAppTarget_Id and --Parent of Current
+			Party_Id is not null
+			)
 
-select * from Party where Id = 'cae7a555-38f8-4038-94b0-c2f450011d7f'
+			
+			Update #PartyRole
+			SET Id = newId() ,
+			InsuranceApplication_Id = @InAppTarget_Id
+			--============== End insert to #PartyRole
 
---============ Find the people not in B
-select * 
-INTO #PartyRole
-from PartyRole 
-where InsuranceApplication_Id = 'DCAB27EB-C824-456E-AA44-83AB17DA402C' and --Insurpolicy
-Party_Id is not null and
-[Type_Id] is not null
-and Party_Id not in 
-(
-select Party_Id 
-from PartyRole 
-where InsuranceApplication_Id = '64e2844f-62fc-4785-aa9c-6f7726975d39' and --BaseInsurancePolicy
-Party_Id is not null
-)
+			--================== InsertData To #InsuranceApplicationItem
 
+			select c.*
+			INTO #InsuranceApplicationItem
+			from InsuranceApplicationRoleItem a
+			inner join PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
+			inner join InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
+			where b.InsuranceApplication_Id = @InAppSource_Id
+			and Party_Id  not in 
+			(
+				select Party_Id 
+				from PartyRole 
+				where InsuranceApplication_Id = @InAppTarget_Id and --BaseInsurancePolicy
+				Party_Id is not null
+			)
 
-Update #PartyRole
-SET Id = newId() ,
-InsuranceApplication_Id = '64e2844f-62fc-4785-aa9c-6f7726975d39'
+				--============= Create InsuranceApplicationItem Mapping ID
+				select c.Id as Id, newId() as NewId, c.Discriminator
+				INTO #InsuranceApplicationItem_Map
+				from InsuranceApplicationRoleItem a
+				inner join PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
+				inner join InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
+				where b.InsuranceApplication_Id = @InAppSource_Id
+				and Party_Id  not in 
+				(
+					select Party_Id 
+					from PartyRole 
+					where InsuranceApplication_Id = @InAppTarget_Id and --BaseInsurancePolicy
+					Party_Id is not null
+				)
 
-select * 
-from #PartyRole a
-UNION
-select * 
-from PartyRole 
-where InsuranceApplication_Id = '64e2844f-62fc-4785-aa9c-6f7726975d39' and --BaseInsurancePolicy
-Party_Id is not null
-order by SequenceNo
---================== End
+			
+			DECLARE @parentId uniqueidentifier 
+			select @parentId = [NewId] from #InsuranceApplicationItem_Map where Discriminator = 'InsuranceApplicationItem'
 
---================== InsertData To #InsuranceApplicationItem
-drop table  #InsuranceApplicationItem
-drop table  #InsuranceApplicationItem_Map
+			Update #InsuranceApplicationItem
+			SET Id = @parentId,
+			InsuranceApplication_Id = NULL,
+			Parent_Id = NULL
+			where Discriminator = 'InsuranceApplicationItem' 
 
-select c.*
-INTO #InsuranceApplicationItem
-from InsuranceApplicationRoleItem a
-inner join PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
-inner join InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
-where b.InsuranceApplication_Id = 'DCAB27EB-C824-456E-AA44-83AB17DA402C'
-and Party_Id  not in 
-(
-select Party_Id 
-from PartyRole 
-where InsuranceApplication_Id = '64e2844f-62fc-4785-aa9c-6f7726975d39' and --BaseInsurancePolicy
-Party_Id is not null
-)
+			Update #InsuranceApplicationItem 
+			SET Id = a.[NewId],
+			Parent_Id = @parentId,
+			InsuranceApplication_Id = @InAppTarget_Id 
+			FROM #InsuranceApplicationItem_Map a
+			WHERE #InsuranceApplicationItem.Id = a.Id
+			AND a.Discriminator = 'InsuranceApplicationPackageItem' 
 
-select c.Id as Id, newId() as NewId, c.Discriminator
-INTO #InsuranceApplicationItem_Map
-from InsuranceApplicationRoleItem a
-inner join PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
-inner join InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
-where b.InsuranceApplication_Id = 'DCAB27EB-C824-456E-AA44-83AB17DA402C'
-and Party_Id  not in 
-(
-select Party_Id 
-from PartyRole 
-where InsuranceApplication_Id = '64e2844f-62fc-4785-aa9c-6f7726975d39' and --BaseInsurancePolicy
-Party_Id is not null
-)
+			
+			--================== End Insert in To #InsuranceApplicationItem
 
-select * 
-from #InsuranceApplicationItem
-where Discriminator = 'InsuranceApplicationItem'
+			--================== InsertData To #InsuranceApplicationRoleItem
+			select a.*
+			INTO #InsuranceApplicationRoleItem
+			from InsuranceApplicationRoleItem a
+			inner join PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
+			inner join InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
+			where b.InsuranceApplication_Id = @InAppSource_Id
+			and Party_Id not in 
+			(
+				select Party_Id 
+				from PartyRole 
+				where InsuranceApplication_Id = @InAppTarget_Id and --BaseInsurancePolicy
+				Party_Id is not null
+			)
 
+			Update #InsuranceApplicationRoleItem
+			SET Id = newId(),
+			InsuranceApplicationRole_Id = c.Id
+			FROM PartyRole a
+			INNER JOIN #PartyRole c on (a.Party_Id = c.Party_Id)
+			WHERE #InsuranceApplicationRoleItem.InsuranceApplicationRole_Id = a.Id 
+			and a.Party_Id in 
+			(
+				select b.Party_Id
+				from InsuranceApplicationRoleItem a
+				inner join PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
+				inner join InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
+				where b.InsuranceApplication_Id = @InAppSource_Id
+				and Party_Id not in 
+				(
+					select Party_Id 
+					from PartyRole 
+					where InsuranceApplication_Id = @InAppTarget_Id and --BaseInsurancePolicy
+					Party_Id is not null
+				)
+			)
 
-select * 
-from #InsuranceApplicationItem
-where Discriminator = 'InsuranceApplicationPackageItem'
+			Update #InsuranceApplicationRoleItem
+			SET InsuranceApplicationItem_Id = a.[NewId]
+			FROM #InsuranceApplicationItem_Map a
+			WHERE #InsuranceApplicationRoleItem.InsuranceApplicationItem_Id = a.Id
 
+			BEGIN TRY
+				BEGIN TRANSACTION
+					INSERT INTO PartyRole
+					select * from #PartyRole
 
+					INSERT INTO InsuranceApplicationItem
+					select * from #InsuranceApplicationItem
 
-DECLARE @parentId uniqueidentifier 
-select @parentId = [NewId] from #InsuranceApplicationItem_Map where Discriminator = 'InsuranceApplicationItem'
+					INSERT INTO InsuranceApplicationRoleItem
+					select * from #InsuranceApplicationRoleItem
+				COMMIT TRANSACTION
+			END TRY
 
-Update #InsuranceApplicationItem
-SET Id = @parentId,
-InsuranceApplication_Id = NULL,
-Parent_Id = NULL
-where Discriminator = 'InsuranceApplicationItem' 
+			BEGIN CATCH
+				SELECT  
+					ERROR_NUMBER() AS ErrorNumber  
+					,ERROR_SEVERITY() AS ErrorSeverity  
+					,ERROR_STATE() AS ErrorState  
+					,ERROR_PROCEDURE() AS ErrorProcedure  
+					,ERROR_MESSAGE() AS ErrorMessage; 
 
-Update #InsuranceApplicationItem 
-SET Id = a.[NewId],
-Parent_Id = @parentId,
-InsuranceApplication_Id = '64e2844f-62fc-4785-aa9c-6f7726975d39' 
-FROM #InsuranceApplicationItem_Map a
-WHERE #InsuranceApplicationItem.Id = a.Id
-AND a.Discriminator = 'InsuranceApplicationPackageItem' 
-
-
-
---================== End
-
-select a.*
-from InsuranceApplicationRoleItem a
-inner join PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
-inner join InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
-where b.InsuranceApplication_Id = 'DCAB27EB-C824-456E-AA44-83AB17DA402C'
-order by SequenceNo, InsuranceApplicationRole_Id
---================== InsertData To #InsuranceApplicationRoleItem
-select a.*
-INTO #InsuranceApplicationRoleItem
-from InsuranceApplicationRoleItem a
-inner join PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
-inner join InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
-where b.InsuranceApplication_Id = 'DCAB27EB-C824-456E-AA44-83AB17DA402C'
-and Party_Id not in 
-(
-select Party_Id 
-from PartyRole 
-where InsuranceApplication_Id = '64e2844f-62fc-4785-aa9c-6f7726975d39' and --BaseInsurancePolicy
-Party_Id is not null
-)
-
-select * from #InsuranceApplicationRoleItem
-
-Update #InsuranceApplicationRoleItem
-SET Id = newId(),
-InsuranceApplicationRole_Id = c.Id
-FROM PartyRole a
-INNER JOIN #PartyRole c on (a.Party_Id = c.Party_Id)
-WHERE #InsuranceApplicationRoleItem.InsuranceApplicationRole_Id = a.Id 
-and a.Party_Id in 
-(
-	select b.Party_Id
-	from InsuranceApplicationRoleItem a
-	inner join PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
-	inner join InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
-	where b.InsuranceApplication_Id = 'DCAB27EB-C824-456E-AA44-83AB17DA402C'
-	and Party_Id not in 
-	(
-		select Party_Id 
-		from PartyRole 
-		where InsuranceApplication_Id = '64e2844f-62fc-4785-aa9c-6f7726975d39' and --BaseInsurancePolicy
-		Party_Id is not null
-	)
-)
-
-Update #InsuranceApplicationRoleItem
-SET InsuranceApplicationItem_Id = a.[NewId]
-FROM #InsuranceApplicationItem_Map a
-WHERE #InsuranceApplicationRoleItem.InsuranceApplicationItem_Id = a.Id
-
-drop table #InsuranceApplicationRoleItem
---================== End
-select b.SequenceNo, count(1)
-from #InsuranceApplicationRoleItem a
-inner join #PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
-inner join #InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
-where b.InsuranceApplication_Id = '64e2844f-62fc-4785-aa9c-6f7726975d39' 
-group by SequenceNo
-order by SequenceNo
-
-
-select b.SequenceNo, count(1)
-from InsuranceApplicationRoleItem a
-inner join PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
-inner join InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
-where b.InsuranceApplication_Id = '64e2844f-62fc-4785-aa9c-6f7726975d39'
-group by SequenceNo
-order by SequenceNo
-
-
-
-select b.SequenceNo, count(1)
-from InsuranceApplicationRoleItem a
-inner join PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
-inner join InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
-where b.InsuranceApplication_Id = 'DCAB27EB-C824-456E-AA44-83AB17DA402C'
-group by SequenceNo
-order by SequenceNo
-
-
-begin tran
-
-INSERT INTO PartyRole
-select * from #PartyRole
-
-INSERT INTO InsuranceApplicationItem
-select * from #InsuranceApplicationItem
-
-INSERT INTO InsuranceApplicationRoleItem
-select * from #InsuranceApplicationRoleItem
-
-rollback
---commit
-
-select *
-from #InsuranceApplicationRoleItem
-where Id not in 
-(
-select a.Id
-from #InsuranceApplicationRoleItem a
-inner join #PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
-inner join #InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
-where b.InsuranceApplication_Id = '64e2844f-62fc-4785-aa9c-6f7726975d39' 
-)
-
-select * from #PartyRole where Id = '931AFE20-1909-4968-8DCF-A71C0A109C1C'
-select * from InsuranceApplicationItem where Id ='37FCEF37-FFE2-4931-BE15-4A33B4574DFF'
-select * from #InsuranceApplicationItem where Id = '0C547F9D-E530-4FD4-97E9-8FEE12C1E1452'
-select * from #InsuranceApplicationItem_Map where [NewId] = '0C547F9D-E530-4FD4-97E9-8FEE12C1E145'
-
-select b.SequenceNo, count(1)
-from InsuranceApplicationRoleItem a
-inner join PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
-inner join InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
-where b.InsuranceApplication_Id = 'DCAB27EB-C824-456E-AA44-83AB17DA402C'
-group by SequenceNo
-order by SequenceNo
-
-select a.*
-from InsuranceApplicationRoleItem a
-inner join PartyRole b on (a.InsuranceApplicationRole_Id = b.Id)
-inner join InsuranceApplicationItem c on (a.InsuranceApplicationItem_Id = c.Id)
-where b.InsuranceApplication_Id = 'DCAB27EB-C824-456E-AA44-83AB17DA402C'
-order by SequenceNo
-
-
-select * 
-from InsuranceApplicationItem
-WHERE InsuranceApplication_Id = 'DCAB27EB-C824-456E-AA44-83AB17DA402C' --Insurpolicy
+				IF @@TRANCOUNT > 0
+					ROLLBACK
+			END CATCH
+			
+			drop table #InsuranceApplicationRoleItem
+			drop table #InsuranceApplicationItem
+			drop table #PartyRole
+	END
